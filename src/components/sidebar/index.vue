d2202acfccd71520eb6d8703f8bc0177db6c5de8
mehrabsha <mehrab572@gmail.com>
2021-01-27 11:30:52 +0330

    complete sidebar

--- src/components/sidebar/index.vue
+++ src/components/sidebar/index.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="sidebar">
+    <Logo />
     <ul>
       <router-link
         v-for="(item, index) in sidebarItems"
         :key="index"
+        :event="item.route != '#' ? 'click' : ''"
         :to="{ name: item.route }"
-        ><li :class="['py-3 pl-4', $route.name == item.route ? 'active' : '']">
-          <font-awesome-icon :icon="item.icon" class="mr-4" />{{ item.title }}
+        tag="div"
+      >
+        <hr v-if="item.title == 'hr'" />
+        <li
+          v-else
+          :class="['py-3 pl-4', $route.name == item.route ? 'active' : '']"
+        >
+          <font-awesome-icon :icon="item.icon" class="mr-4" /><span
+            class="pr-5 mr-4 d-none d-md-inline"
+            >{{ item.title }}</span
+          >
         </li>
       </router-link>
     </ul>
@@ -14,6 +25,7 @@
 </template>
 
 <script>
+import Logo from './logo'
 export default {
   data() {
     return {
@@ -28,8 +40,44 @@ export default {
           route: 'patients',
           icon: 'ticket-alt',
         },
+        {
+          title: 'Link number2',
+          route: '#',
+          icon: 'lightbulb',
+        },
+        {
+          title: 'Link number3',
+          route: '#',
+          icon: 'users',
+        },
+        {
+          title: 'Link number4',
+          route: '#',
+          icon: 'user-tie',
+        },
+        {
+          title: 'Link number5',
+          route: '#',
+          icon: 'book',
+        },
+        {
+          title: 'hr',
+        },
+        {
+          title: 'Settings',
+          route: '#',
+          icon: 'cog',
+        },
+        {
+          title: 'Logout',
+          route: '#',
+          icon: 'award',
+        },
       ],
     }
+  },
+  components: {
+    Logo,
   },
 }
 </script>
@@ -37,34 +85,33 @@ export default {
 <style lang="scss">
 .sidebar {
   background-color: #363740;
-  width: 255px;
+  width: fit-content;
   height: 100vh;
-
+  hr {
+    background-color: #9fa2b4;
+  }
   ul {
     list-style: none;
     padding: 0;
     .active {
       background-color: rgba($color: #9fa2b4, $alpha: 0.08);
-      border-left: 4px solid #dde2ff;
+      border-left: 4px solid rgba($color: #dde2ff, $alpha: 1);
       border-radius: 2px 0px 0px 2px;
-      a {
-        color: #dde2ff !important;
-      }
+      color: #dde2ff !important;
       svg {
         color: #dde2ff;
       }
     }
     li {
+      border-left: 4px solid rgba($color: #dde2ff, $alpha: 0);
       transition: 0.2s;
       cursor: pointer;
       svg {
         color: #9fa2b4;
+        width: 20px !important;
       }
     }
   }
-  a {
-    color: #a4a6b3;
-    text-decoration: none !important;
-  }
+  color: #a4a6b3;
 }
 </style>
---
+++ src/components/sidebar/logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="logo py-4 mt-4 mb-5">
+    <img src="@/assets/img/logo.png" alt="" />
+    <h5 class="ml-3 mb-0 mt-1  d-none d-md-inline">Covid 19 Tracker</h5>
+  </div>
+</template>
+
+<style lang="scss">
+.logo {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  font-weight: bold !important;
+}
+</style>
